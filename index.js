3d85d3c55928f5e0098c499853217418b1bd9d50
mwichabe <mwichabecollins@gmail.com>
2025-01-18 11:34:46 +0300

    Register API done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoute")
+
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",userRoutes);
+
+//mongoDb
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(
+    ()=>{
+        console.log(`DB Connected successfully`)
+    }
+).catch((error)=>{
+    console.log(error.message)
+});
+
+const server= app.listen(process.env.PORT,()=>{
+    console.log(`Server started on Port ${process.env.PORT}`)
+})
+
